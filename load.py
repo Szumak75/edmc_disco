b9e28e57bc41fb1b4719f55a33fd7c7578457680
Jacek Kotlarski <szumak@virthost.pl>
2024-01-23 20:31:58 +0100

    main app button

--- load.py
+++ load.py
@@ -10,6 +10,7 @@ import tkinter as tk
 from tkinter import ttk
 from typing import Dict, Optional, Tuple
 
+from jsktoolbox.tktool.widgets import CreateToolTip
 
 from config import config
 
@@ -50,34 +51,42 @@ def plugin_stop() -> None:
         )
     disco_object.data.shutting_down = True
     if disco_object.logger:
-        disco_object.logger.debug = f"{disco_object.data.pluginname}->plugin_stop: shut down flag is set"
+        disco_object.logger.debug = (
+            f"{disco_object.data.pluginname}->plugin_stop: shut down flag is set"
+        )
     # something to do
 
     disco_object.data.db_processor.close()
 
     # shut down logger at last
     if disco_object.logger:
-        disco_object.logger.debug = f"{disco_object.data.pluginname}->plugin_stop: terminating the logger"
+        disco_object.logger.debug = (
+            f"{disco_object.data.pluginname}->plugin_stop: terminating the logger"
+        )
     disco_object.qlog.put(None)
     disco_object.thlog.join()
     disco_object.thlog = None  # type: ignore
 
 
-def plugin_app(parent: tk.Frame) -> Tuple[tk.Label, ttk.Button]:
+def plugin_app(parent: tk.Frame) -> ttk.Button:
     """Create a pair of TK widgets for the EDMarketConnector main window.
 
     parent:     The root EDMarketConnector window
     """
-    label = tk.Label(
-        parent,
-        text=f"{disco_object.data.pluginname} v{disco_object.data.version}:",
-    )
     if disco_object.data.dialog is None:
         disco_object.data.dialog = DiscoMainDialog(
             parent, disco_object.qlog, disco_object.data
         )
-    button = disco_object.data.dialog.button()
-    return label, button
+    button = disco_object.data.dialog.button()  # type: ignore
+    CreateToolTip(
+        button,
+        [
+            f"{disco_object.data.pluginname} v{disco_object.data.version}:",
+            "",
+            "Show or search for discovered system data.",
+        ],
+    )
+    return button
 
 
 def prefs_changed(cmdr: str, is_beta: bool) -> None:
@@ -91,9 +100,7 @@ def prefs_changed(cmdr: str, is_beta: bool) -> None:
     )
     # set loglevel after config update
     disco_object.log_processor.loglevel = LogLevels().get(config.get_str("loglevel"))  # type: ignore
-    disco_object.logger.debug = (
-        f"{disco_object.data.pluginname}->prefs_changed: done."
-    )
+    disco_object.logger.debug = f"{disco_object.data.pluginname}->prefs_changed: done."
 
 
 def journal_entry(
@@ -131,22 +138,16 @@ def journal_entry(
     elif entry["event"] == "FSSDiscoveryScan":
         disco_object.data.system = disco_object.data.db_processor.update_system(entry)  # type: ignore
         test = True
-        disco_object.logger.debug = (
-            f"FSSDiscoveryScan: {disco_object.data.system}"
-        )
+        disco_object.logger.debug = f"FSSDiscoveryScan: {disco_object.data.system}"
     elif entry["event"] == "FSSBodySignals":
         disco_object.data.system = disco_object.data.db_processor.add_signal(entry)  # type: ignore
         test = True
-        disco_object.logger.debug = (
-            f"FSSBodySignals: {disco_object.data.system}"
-        )
+        disco_object.logger.debug = f"FSSBodySignals: {disco_object.data.system}"
     elif entry["event"] == "SAASignalsFound":
         disco_object.data.db_processor.add_signal(entry)
         disco_object.data.system = disco_object.data.db_processor.add_genus(entry)  # type: ignore
         test = True
-        disco_object.logger.debug = (
-            f"SAASignalsFound: {disco_object.data.system}"
-        )
+        disco_object.logger.debug = f"SAASignalsFound: {disco_object.data.system}"
     elif entry["event"] == "CodexEntry":
         disco_object.data.system = disco_object.data.db_processor.add_codex(entry)  # type: ignore
         test = True
@@ -154,17 +155,13 @@ def journal_entry(
     elif entry["event"] == "ScanOrganic":
         disco_object.data.system = disco_object.data.db_processor.add_genus(entry)  # type: ignore
         test = True
-        disco_object.logger.debug = (
-            f"ScanOrganic: {disco_object.data.system}"
-        )
+        disco_object.logger.debug = f"ScanOrganic: {disco_object.data.system}"
     elif entry["event"] == "SAAScanComplete":
         disco_object.data.system = disco_object.data.db_processor.mapped_body(entry)  # type: ignore
         test = True
     elif entry["event"] == "Location" and "StarSystem" in entry:
-        disco_object.data.system = (
-            disco_object.data.db_processor.get_system_by_name(
-                entry["StarSystem"]
-            )
+        disco_object.data.system = disco_object.data.db_processor.get_system_by_name(
+            entry["StarSystem"]
         )  # type: ignore
         test = True
     if test:
