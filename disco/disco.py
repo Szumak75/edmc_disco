74d429d562e9710674dbb39641d4d8dfc93a6bca
Jacek Kotlarski <szumak@virthost.pl>
2024-10-18 16:17:13 +0200

    version up

--- disco/disco.py
+++ disco/disco.py
@@ -26,7 +26,7 @@ class Disco(BLogProcessor, BLogClient):
 
         # data
         self.data.plugin_name = "EDDisco"
-        self.data.version = "1.1.2"
+        self.data.version = "1.1.3-dev"
 
         # database
         self.data.db_processor = DBProcessor(Database(False).session)
